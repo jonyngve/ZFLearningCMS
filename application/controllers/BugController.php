fefcb80a3b04237a73f91609a306bd63b845dea9
Jon-Yngve Skjeltorp Bakke <jonyngve@skjeltorpbakke.com>
2011-07-04 22:43:56 +0200

    Bug form styles and filter

--- application/controllers/BugController.php
+++ application/controllers/BugController.php
@@ -58,10 +58,33 @@ class BugController extends Zend_Controller_Action
 
     public function listAction()
     {
+        // get the filter form
+        $listToolsForm = new Form_BugReportListToolsForm();
+
+        // set the default values to null
+        $sort = null;
+        $filter = null;
+
+        // if this request is a postback and is valid, then add the sort
+        // filter criteria
+        if ($this->getRequest()->isPost()) {
+            if ($listToolsForm->isValid($_POST)) {
+                $sortValue = $listToolsForm->getValue('sort');
+                if ($sortValue != '0') {
+                    $sort = $sortValue;
+                }
+                $filterFieldValue = $listToolsForm->getValue('filter_field');
+                if ($filterFieldValue != '0') {
+                    $filter[$filterFieldValue] = $listToolsForm->getValue('filter');
+                }
+            }
+        }
+
+        // fetch the current bugs
         $bugModel = new Model_Bug();
-        $this->view->bugs = $bugModel->fetchBugs();
+        $this->view->bugs = $bugModel->fetchBugs($filter, $sort);
+        $listToolsForm->setAction('/bug/list');
+        $listToolsForm->setMethod('post');
+        $this->view->listToolsForm = $listToolsForm;
     }
-
-
 }
-
